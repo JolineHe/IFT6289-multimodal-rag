ce53d4c1d6109b45c2284283d82ad29f9fb9b0f3
Qiaoling He <qiaoling2000@gmail.com>
2025-03-21 18:00:25 -0400

    add vector_search_mongodb and rag_agent, test passed

--- src/rag_agent.py
+++ src/rag_agent.py
@@ -1,9 +1,11 @@
 from typing import List, Optional
+from pymongo import MongoClient
 from pydantic import BaseModel
 from data_models import Address
-from vector_search import vector_search
+from vector_search_mongodb import VectorSearchMongoDB
 import pandas as pd
 import openai
+import os
 
 
 class SearchResultItem(BaseModel):
@@ -16,55 +18,70 @@ class SearchResultItem(BaseModel):
     notes: Optional[str] = None
 
 
-def handle_user_query(query, db, collection):
-    # Assuming vector_search returns a list of dictionaries with keys 'title' and 'plot'
-    get_knowledge = vector_search(query, db, collection)
+class RagAgent:
+    def __init__(self, db, collection, vector_search):
+        self.db = db
+        self.collection = collection
+        self.vector_search = vector_search    
 
-    # Check if there are any results
-    if not get_knowledge:
-        return "No results found.", "No source information available."
-        
-     # Convert search results into a list of SearchResultItem models
-    search_results_models = [
-        SearchResultItem(**result)
-        for result in get_knowledge
-    ]
+    def handle_user_query(self, query):
+        # Assuming vector_search returns a list of dictionaries with keys 'title' and 'plot'
+        get_knowledge = self.vector_search.do_vector_search(query)
 
-    # Convert search results into a DataFrame for better rendering in Jupyter
-    search_results_df = pd.DataFrame([item.dict() for item in search_results_models])
-
-    # Generate system response using OpenAI's completion
-    completion = openai.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system", 
-                "content": "You are a airbnb listing recommendation system."},
-            {
-                "role": "user", 
-                "content": f"Answer this user query: {query} with the following context:\n{search_results_df}"
-            }
+        # Check if there are any results
+        if not get_knowledge:
+            return "No results found.", "No source information available."
+            
+        # Convert search results into a list of SearchResultItem models
+        search_results_models = [
+            SearchResultItem(**result)
+            for result in get_knowledge
         ]
-    )
 
-    system_response = completion.choices[0].message.content
+        # Convert search results into a DataFrame for better rendering in Jupyter
+        search_results_df = pd.DataFrame([item.dict() for item in search_results_models])
 
-    # Print User Question, System Response, and Source Information
-    print(f"- User Question:\n{query}\n")
-    print(f"- System Response:\n{system_response}\n")
+        # Generate system response using OpenAI's completion
+        completion = openai.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system", 
+                    "content": "You are a airbnb listing recommendation system."},
+                {
+                    "role": "user", 
+                    "content": f"Answer this user query: {query} with the following context:\n{search_results_df}"
+                }
+            ]
+        )
 
-    # Display the DataFrame as an HTML table
-    display(HTML(search_results_df.to_html()))
+        system_response = completion.choices[0].message.content
 
-    # Return structured response and source info as a string
-    return system_response
+        # Print User Question, System Response, and Source Information
+        print(f"- User Question:\n{query}\n")
+        print(f"- System Response:\n{system_response}\n")
+
+        # Return structured response and source info as a string
+        return system_response
 
 
 
 if __name__ == "__main__":
+    uri = os.getenv('MONGODB_URI')
+    client = MongoClient(uri)
+
+    db_name = 'airbnb_dataset'
+    collection_name = 'airbnb_embeddings'
+
+    db = client[db_name]
+    collection = db[collection_name]
+
+    vector_search = VectorSearchMongoDB(db, collection)
+    rag_agent = RagAgent(db, collection, vector_search)
+
     query = """
     I want to stay in a place that's warm and friendly, 
     and not too far from resturants, can you recommend a place? 
     Include a reason as to why you've chosen your selection.
     """
-    handle_user_query(query, db, collection)
+    rag_agent.handle_user_query(query)
---
+++ src/vector_search_mongodb.py
@@ -0,0 +1,120 @@
+from pymongo.operations import SearchIndexModel
+from pymongo import MongoClient
+import time
+import openai
+import os
+from dotenv import load_dotenv
+
+text_embedding_field_name = "text_embeddings"
+vector_search_index_name_text = "vector_index_text"
+
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+class VectorSearchMongoDB:
+    def __init__(self, db, collection):
+        self.db = db
+        self.collection = collection
+
+    def create_vector_search_index(self):
+        vector_search_index_model = SearchIndexModel(
+            definition={
+                "mappings": {
+                    "dynamic": True,
+                    "fields": {
+                        text_embedding_field_name: {
+                            "dimensions": 1536,
+                            "similarity": "cosine",
+                            "type": "knnVector",
+                        }
+                    },
+                }
+            },
+            name=vector_search_index_name_text,
+        )
+
+        index_exists = False
+        for index in self.collection.list_indexes():
+            if index['name'] == vector_search_index_name_text:
+                index_exists = True
+                break
+
+        if not index_exists:
+            try:
+                result = self.collection.create_search_index(model=vector_search_index_model)
+                print("Creating index...")
+                time.sleep(20)
+                print("Index created successfully:", result)
+                print("Wait a few minutes before conducting search with index to ensure index intialization")
+            except Exception as e:
+                print(f"Error creating vector search index: {str(e)}")
+        else:
+            print(f"Index '{vector_search_index_name_text}' already exists.")
+
+    def get_embedding(self, text):
+        if not text or not isinstance(text, str):
+            return None
+
+        try:
+            embedding = openai.embeddings.create(
+                input=text,
+                model="text-embedding-3-small", dimensions=1536).data[0].embedding
+            return embedding
+        except Exception as e:
+            print(f"Error in get_embedding: {e}")
+            return None
+
+    def do_vector_search(self, user_query, vector_index="vector_index_text"):
+        query_embedding = self.get_embedding(user_query)
+
+        if query_embedding is None:
+            return "Invalid query or embedding generation failed."
+
+        vector_search_stage = {
+            "$vectorSearch": {
+                "index": vector_index,
+                "queryVector": query_embedding,
+                "path": text_embedding_field_name,
+                "numCandidates": 150,
+                "limit": 20
+            }
+        }
+
+        pipeline = [vector_search_stage]
+
+        results = self.collection.aggregate(pipeline)
+
+        explain_query_execution = self.db.command(
+            'explain',
+            {
+                'aggregate': self.collection.name,
+                'pipeline': pipeline,
+                'cursor': {}
+            },
+            verbosity='executionStats')
+
+        vector_search_explain = explain_query_execution['stages'][0]['$vectorSearch']
+        millis_elapsed = vector_search_explain['explain']['collectors']['allCollectorStats']['millisElapsed']
+
+        print(f"Total time for the execution to complete on the database server: {millis_elapsed} milliseconds")
+
+        return list(results)
+
+
+
+if __name__ == "__main__":
+    uri = os.getenv('MONGODB_URI')
+    client = MongoClient(uri)
+
+    db_name = 'airbnb_dataset'
+    collection_name = 'airbnb_embeddings'
+
+    db = client[db_name]
+    collection = db[collection_name]
+
+    vector_search_mongodb = VectorSearchMongoDB(db, collection)
+    # vector_search_mongodb.create_vector_search_index()
+    results = vector_search_mongodb.do_vector_search("luxury apartment in the heart of downtown Montreal")
+    print(results)
